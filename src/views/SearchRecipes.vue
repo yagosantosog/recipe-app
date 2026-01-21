f72ef699d79837aa122ddb4b6a7553e2f63d5b74
Yago Santos <santosyago2005@gmail.com>
2026-01-20 22:13:12 -0300

    style: allow search result content to fit better in grid

--- src/views/SearchRecipes.vue
+++ src/views/SearchRecipes.vue
@@ -137,7 +137,7 @@ input:focus {
 
 .searchRecipes__grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(15rem, 20rem));
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 2rem;
   align-items: center;
   justify-content: center;
