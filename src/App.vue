abf6b6b695eff71487e65388790c515cfbde684b
Yago Santos <santosyago2005@gmail.com>
2026-01-20 22:50:16 -0300

    refactor: separate header in a layout component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
 <script setup>
-import { RouterView, useRouter } from 'vue-router'
-
-const router = useRouter()
+import { RouterView } from 'vue-router'
+import MainLayout from './components/layouts/MainLayout.vue'
 </script>
 
 <template>
-  <header>
-    <div class="header__container container">
-      <div @click="router.push('/')" class="logo">
-        <img src="./assets/logo.webp" alt="logo" />
-      </div>
-    </div>
-  </header>
-  <div class="container">
+  <MainLayout>
     <RouterView />
-  </div>
+  </MainLayout>
 </template>
 
-<style scoped>
-header {
-  background-color: hsl(var(--bg-dark));
-}
-
-.logo {
-  width: 25vw;
-  max-width: 10rem;
-  margin-inline: auto;
-}
-
-@media (min-width: 85rem) {
-  .logo {
-    margin: unset;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+<template>
+  <header>
+    <div class="header__container container">
+      <button @click="router.push('/')" class="logo">
+        <img src="../../assets/logo.webp" alt="logo" />
+      </button>
+    </div>
+  </header>
+  <slot />
+</template>
+<style scoped>
+img {
+  max-width: 10vw;
+}
+</style>
